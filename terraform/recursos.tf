d9e316ab71f5e4430fc844d7e0f36021736622da
Papol <contactopabloaragon@gmail.com>
2026-02-27 10:46:39 +0100

    puerto 443 para el servidor de nginx

--- terraform/recursos.tf
+++ terraform/recursos.tf
@@ -64,13 +64,13 @@ resource "azurerm_network_security_group" "nsg" {
   }
 
   security_rule {
-    name                       = "allow-https-8443"
+    name                       = "allow-https"
     priority                   = 120
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "8443"
+    destination_port_range     = "443"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
