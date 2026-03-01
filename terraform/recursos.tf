1427c7354ba17b51313f68e014add8df9ea14f57
Papol <contactopabloaragon@gmail.com>
2026-03-01 19:43:28 +0100

    cambiar regla del puerto 443 al 8443

--- terraform/recursos.tf
+++ terraform/recursos.tf
@@ -64,13 +64,13 @@ resource "azurerm_network_security_group" "nsg" {
   }
 
   security_rule {
-    name                       = "allow-https"
+    name                       = "allow-https-8443"
     priority                   = 120
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "443"
+    destination_port_range     = "8443"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
